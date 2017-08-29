e8842682090d8618b92c69010e5dcf0155f92c1e
rafatq <RApollo@gmail.com>
2017-08-29 14:49:37 +0200

    Add uploading file

--- src/main/java/com/motolok/app/AppApplication.java
+++ src/main/java/com/motolok/app/AppApplication.java
@@ -3,8 +3,17 @@ package com.motolok.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @SpringBootApplication
@@ -15,6 +24,35 @@ public class AppApplication {
 	String home() {
 		return "Hello World!";
 	}
+
+
+	@PostMapping("/upload") // //new annotation since 4.3
+	@ResponseBody
+	public String singleFileUpload(@RequestParam("file") MultipartFile file,
+								   RedirectAttributes redirectAttributes) {
+
+		if (file.isEmpty()) {
+//			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+			return "rPlease select a file to upload";
+		}
+
+		try {
+
+			// Get the file and save it somewhere
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get("./" + file.getOriginalFilename());
+			Files.write(path, bytes);
+
+//			redirectAttributes.addFlashAttribute("message",
+//					"You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:/uploadStatus";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
