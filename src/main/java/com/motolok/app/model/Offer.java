fddac500aaf0077a9920e4772ba743bf0f5b5a43
rafatq <RApollo@gmail.com>
2017-08-29 23:54:50 +0200

    Add new properties to offer model

--- src/main/java/com/motolok/app/model/Offer.java
+++ src/main/java/com/motolok/app/model/Offer.java
@@ -12,11 +12,23 @@ public class Offer {
     @GeneratedValue
     private Long id;
     @Column
-    private String name;
+    private String model;
     @Column
     private String description;
     @Column
+    private String phoneNumber;
+    @Column
     private Double price;
+    @Column
+    private Integer year;
+    @Column
+    private String plate;
+    @Column
+    private String image;
+    @Column
+    private Double latitude;
+    @Column
+    private Double longitude;
 
     public Long getId() {
         return id;
@@ -26,12 +38,12 @@ public class Offer {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getModel() {
+        return model;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setModel(String model) {
+        this.model = model;
     }
 
     public String getDescription() {
@@ -48,5 +60,53 @@ public class Offer {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 }
